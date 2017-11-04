d54a44ab2a412c74f585fade4d9ed5473bdc1f23
Paul Frederiks <p.frederiks@uu.nl>
2017-11-04 05:42:15 +0100

    Pull in all the other third party microservices with fixes. This stops the double-linked list crashes

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # irods-uu-microservices
-Miscellaneous microservices developed by Utrecht university.
+Miscellaneous iRODS microservices developed or modified by Utrecht university.
+
+
+msi\_json\_arrayops and msi\_json\_objops microservices are derived from
+work from the Donders Institute. The license below applies
+
+### Copyright and license for msi_json_arrayops and msi_json_objops
+
+This microservice has kindly been provided by the Donders Institute for
+Brain, Cognition and Behaviour.
+
+Copyright (c) 2016, Radboud University, Nijmegen, The Netherlands
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ src/msi_json_arrayops.cc
@@ -0,0 +1,131 @@
+// =-=-=-=-=-=-=-
+#include "apiHeaderAll.hpp"
+#include "msParam.hpp"
+#include "reGlobalsExtern.hpp"
+#include "irods_ms_plugin.hpp"
+#include "reFuncDefs.hpp"
+#include "jansson.h"
+
+// =-=-=-=-=-=-=-
+// STL/boost Includes
+#include <string>
+#include <iostream>
+#include <vector>
+//#include <boost/algorithm/string.hpp>
+
+extern "C" {
+    // =-=-=-=-=-=-=-
+    int msi_json_arrayops_impl(msParam_t* json_str, msParam_t* val, msParam_t* ops, msParam_t* sizeOrIndex, ruleExecInfo_t* rei) {
+        using std::cout;
+        using std::endl;
+        using std::string;
+
+        // input type check
+        const char *inJsonStr = parseMspForStr( json_str );
+        const char *inVal = parseMspForStr( val );
+        const char *inOps = parseMspForStr( ops );
+        const int inIdx = parseMspForPosInt( sizeOrIndex );
+
+        if( ! inJsonStr ) {
+            cout << "msi_json_arrayops - invalid inJsonStr" << endl;
+            return SYS_INVALID_INPUT_PARAM;
+        }
+        if( ! inOps ) {
+            cout << "msi_json_arrayops - invalid inOps" << endl;
+            return SYS_INVALID_INPUT_PARAM;
+        }
+        if( ! inVal ) {
+            cout << "msi_json_arrayops - invalid inVal" << endl;
+            return SYS_INVALID_INPUT_PARAM;
+        }
+
+        string strOps(inOps); 
+
+        json_error_t error;
+        json_t *root;
+
+        // try to make initial inJsonStr if it's an empty string 
+        if ( strcmp(inJsonStr, "") == 0 ) {
+            inJsonStr = "[]";
+        }
+
+        // try to load JSON object
+        root = json_loads(inJsonStr, 0, &error);
+        if ( ! root || ! json_is_array(root) ) {
+            cout << "msi_json_arrayops - invalid json document" << endl;
+            json_decref(root);
+            return SYS_INVALID_INPUT_PARAM;
+        }
+
+        int outSizeOrIndex = (int) json_array_size(root);
+        json_t *jval;
+        if (strcmp(inVal, "null") == 0) {
+            jval = json_null();
+        } else if (strcmp(inVal, "true") == 0 ) {
+            jval = json_true();
+        } else if (strcmp(inVal, "false") == 0 ) {
+            jval = json_false();
+        } else {
+            jval = json_loads(inVal, 0, &error);
+            if ( ! jval ) jval = json_string(inVal);
+        }
+
+        // find if jval is already presented in array
+        size_t i_match = outSizeOrIndex;
+        for( int i=0; i < outSizeOrIndex; i++ ) {
+            json_t *ov = json_array_get(root, i);
+            if ( json_equal(ov, jval) ) { i_match = i; break; }
+        }
+
+        if ( strOps == "add" ) {
+            // append value only if it's a boolean, or it's not presented in the array
+            if ( json_is_boolean(jval) || i_match == outSizeOrIndex ) {
+                json_array_append_new(root, jval);
+                outSizeOrIndex = (int) json_array_size(root);
+            }
+        } else if ( strOps == "find" || strOps == "rm" ) {
+
+            if ( i_match < outSizeOrIndex ) {
+                if ( strOps == "rm" ) {
+                    json_array_remove(root, i_match);
+                    outSizeOrIndex = (int) json_array_size(root);
+                } else {
+                    outSizeOrIndex = i_match;
+                }
+            } else if ( strOps == "find" ) {
+                outSizeOrIndex = -1;
+            }
+        } else if ( strOps == "size" ) {
+            outSizeOrIndex = (int) json_array_size(root);
+        } else if ( strOps == "get" ) {
+            json_t *elem = json_array_get(root, inIdx);
+
+	    /* output a string directly, but encode other json types using json_dumps with JSON_ENCODE_ANY set */
+            if ( json_is_string(elem)) { 
+                fillStrInMsParam(val, json_string_value(elem));
+            } else {
+                fillStrInMsParam(val, json_dumps(elem, JSON_ENCODE_ANY));
+            }
+
+
+            outSizeOrIndex = inIdx;
+        }
+
+        fillStrInMsParam(json_str, json_dumps(root, 0));
+        fillIntInMsParam(sizeOrIndex, outSizeOrIndex);
+
+        json_decref(root);
+
+        // Done
+        return 0;
+    }
+
+    irods::ms_table_entry* plugin_factory() {
+        irods::ms_table_entry* msvc = new irods::ms_table_entry(4);
+        
+        msvc->add_operation("msi_json_arrayops_impl", "msi_json_arrayops");
+        
+        return msvc;
+    }
+
+} // extern "C"
