00222ff3ff5f069648a1c47de6c75896fe92c64e
Sietse Snel <s.t.snel@uu.nl>
2024-02-22 14:14:55 +0100

    Adapt build scripts for Ubuntu 20.04 LTS (focal)

--- vagrant/build/install-microservices-build-env.sh
+++ vagrant/build/install-microservices-build-env.sh
@@ -47,9 +47,6 @@ then
 
   echo "Installing iRODS UU microservices build environment on Ubuntu."
 
-  echo "Installing dependencies ..."
-  sudo apt-get -y install aptitude
-
   echo "Downloading and installing iRODS repository signing key ..."
   wget -qO - "$APT_IRODS_REPO_SIGNING_KEY_LOC" | sudo apt-key add -
 
@@ -57,11 +54,36 @@ then
 cat << ENDAPTREPO | sudo tee /etc/apt/sources.list.d/irods.list
 deb [arch=${APT_IRODS_REPO_ARCHITECTURE}] $APT_IRODS_REPO_URL $APT_IRODS_REPO_DISTRIBUTION $APT_IRODS_REPO_COMPONENT
 ENDAPTREPO
-  sudo apt-get update
+  sudo apt update
+
+  echo "Installing dependencies ..."
+  sudo apt-get -y install aptitude libboost-locale-dev
+
+  sudo apt install -y libpython2-stdlib libpython2.7-minimal libpython2.7-stdlib \
+          python-is-python2 python-six python2 python2-minimal python2.7 python2.7-minimal \
+          python-certifi python-chardet python-idna python-pkg-resources python-setuptools
+
+  PY_URLLIB_PREFIX="http://security.ubuntu.com/ubuntu/pool/main/p/python-urllib3"
+  PY_URLLIB_FILENAME="python-urllib3_1.22-1ubuntu0.18.04.2_all.deb"
+  PY_REQUESTS_PREFIX="http://security.ubuntu.com/ubuntu/pool/main/r/requests"
+  PY_REQUESTS_FILENAME="python-requests_2.18.4-2ubuntu0.1_all.deb"
+  OPENSSL_PREFIX="http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0"
+  OPENSSL_FILENAME="libssl1.0.0_1.0.2n-1ubuntu5.13_amd64.deb"
+
+  wget \
+        ${PY_URLLIB_PREFIX}/${PY_URLLIB_FILENAME} \
+        ${PY_REQUESTS_PREFIX}/${PY_REQUESTS_FILENAME} \
+        ${OPENSSL_PREFIX}/${OPENSSL_FILENAME}
+
+  for package in $PY_URLLIB_FILENAME $PY_REQUESTS_FILENAME $OPENSSL_FILENAME
+  do echo "Installing package ${package%.*}"
+     sudo dpkg -i "$package"
+     rm "$package"
+  done
 
   for package in $APT_IRODS_PACKAGES
   do echo "Installing package $package and its dependencies"
-     sudo apt-get -y install "$package=${IRODS_VERSION}"
+     sudo apt-get -y install "$package=${IRODS_VERSION}-1~bionic"
      sudo aptitude hold "$package"
   done
 
