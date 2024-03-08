c5a37720feb7310e991d1e896f447cdf70617ba8
Lazlo Westerhof <l.r.westerhof@uu.nl>
2024-03-08 14:31:37 +0100

    YDA-5243: format with clang-format

--- src/msi_dataObj_checksum.cpp
+++ src/msi_dataObj_checksum.cpp
@@ -1,25 +1,25 @@
 #include "irods/SHA256Strategy.hpp"
 #include "irods/checksum.h"
-#include "rsGenQuery.hpp"
+#include "irods/dataObjInpOut.h"
 #include "irods/rcMisc.h"
 #include "irods/rods.h"
-#include "irods/dataObjInpOut.h"
+#include "rsGenQuery.hpp"
 
 #include "irods_includes.hh"
 
-#include <stdio.h>
-#include <string.h>
 #include <cstring>
 #include <irods_error.hpp>
+#include <stdio.h>
+#include <string.h>
 
-
-int calculate_checksum(rsComm_t *rsComm, std::string const& dataObjInp, std::string replNum, char* outChksum) {
+int calculate_checksum(rsComm_t* rsComm, std::string const& dataObjInp, std::string replNum, char* outChksum)
+{
     char pathQCond[MAX_NAME_LEN];
     char replQCond[MAX_NAME_LEN];
     genQueryInp_t genQueryInp;
-    genQueryOut_t *genQueryOut;
+    genQueryOut_t* genQueryOut;
     char myColl[MAX_NAME_LEN], myData[MAX_NAME_LEN];
-    sqlResult_t *path;
+    sqlResult_t* path;
     std::string phyPath;
     int status;
     int ret = 0;
@@ -45,13 +45,16 @@ int calculate_checksum(rsComm_t *rsComm, std::string const& dataObjInp, std::str
     genQueryOut = NULL;
     status = rsGenQuery(rsComm, &genQueryInp, &genQueryOut);
 
-    if ( status >= 0 ) {
+    if (status >= 0) {
         if (genQueryOut->rowCnt != 1) {
-            rodsLog( LOG_ERROR, "_msi_dataObj_checksum: Unknown File");
+            rodsLog(LOG_ERROR, "_msi_dataObj_checksum: Unknown File");
             ret = CAT_UNKNOWN_FILE;
-        } else {
-            if (( path = getSqlResultByInx( genQueryOut, COL_D_DATA_PATH ) ) == NULL) {
-                rodsLog( LOG_ERROR, "_msi_dataObj_checksum: getSqlResultByInx for COL_D_DATA_PATH failed");
+        }
+        else {
+            if ((path = getSqlResultByInx(genQueryOut, COL_D_DATA_PATH)) == NULL) {
+                rodsLog(LOG_ERROR,
+                        "_msi_dataObj_checksum: getSqlResultByInx for "
+                        "COL_D_DATA_PATH failed");
                 ret = UNMATCHED_KEY_OR_INDEX;
             }
             else {
@@ -60,7 +63,6 @@ int calculate_checksum(rsComm_t *rsComm, std::string const& dataObjInp, std::str
         }
     }
 
-    
     clearGenQueryInp(&genQueryInp);
 
     freeGenQueryOut(&genQueryOut);
@@ -68,53 +70,44 @@ int calculate_checksum(rsComm_t *rsComm, std::string const& dataObjInp, std::str
     int retCode = chksumLocFile(phyPath.c_str(), outChksum, irods::SHA256_NAME.c_str());
 
     if (retCode < 0) {
-        rodsLog( LOG_ERROR, "_msi_dataObj_checksum: Failed to calculate checksum for file: [{}], status = {}", myData, retCode);
+        rodsLog(LOG_ERROR,
+                "_msi_dataObj_checksum: Failed to calculate checksum for file: "
+                "[{}], status = {}",
+                myData,
+                retCode);
         ret = retCode;
     }
 
     return ret;
 }
 
-
 extern "C" {
 
-int msidataObjchecksum(
-    msParam_t* dataObjInp,
-    msParam_t* replNum,
-    msParam_t* statusOut,
-    ruleExecInfo_t* rei ) {
-
-    std::string repl_num = parseMspForStr( replNum );
-    std::string dataObj = parseMspForStr( dataObjInp );
+int msidataObjchecksum(msParam_t* dataObjInp, msParam_t* replNum, msParam_t* statusOut, ruleExecInfo_t* rei)
+{
+    std::string repl_num = parseMspForStr(replNum);
+    std::string dataObj = parseMspForStr(dataObjInp);
     char chksum[NAME_LEN];
 
     int ret = calculate_checksum(rei->rsComm, dataObj, repl_num, chksum);
 
     if (ret < 0) {
-        rodsLog( LOG_ERROR, "_msi_dataObj_checksum: Failed to calculate checksum");
+        rodsLog(LOG_ERROR, "_msi_dataObj_checksum: Failed to calculate checksum");
     }
-    else{
+    else {
         fillStrInMsParam(statusOut, chksum);
     }
 
     return ret;
-     
- }
-
-irods::ms_table_entry* plugin_factory() {
-    irods::ms_table_entry* msvc = new irods::ms_table_entry(3);
-
-    msvc->add_operation<
-        msParam_t*,
-        msParam_t*,
-        msParam_t*,
-        ruleExecInfo_t*>("msidataObjchecksum",
-                         std::function<int(
-                             msParam_t*,
-                             msParam_t*,
-                             msParam_t*,
-                             ruleExecInfo_t*)>(msidataObjchecksum));
-    return msvc;
 }
 
+irods::ms_table_entry* plugin_factory()
+{
+    irods::ms_table_entry* msvc = new irods::ms_table_entry(3);
+
+    msvc->add_operation<msParam_t*, msParam_t*, msParam_t*, ruleExecInfo_t*>(
+        "msidataObjchecksum",
+        std::function<int(msParam_t*, msParam_t*, msParam_t*, ruleExecInfo_t*)>(msidataObjchecksum));
+    return msvc;
+}
 }
